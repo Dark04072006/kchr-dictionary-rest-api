e93d9e4d5246f95f56444cfca3323e15c676c9bb
alim <Abrekovalim38702@gmail.com>
2024-04-17 02:15:13 +0300

    alembic migrate

--- scripts/start.dev.sh
+++ scripts/start.dev.sh
@@ -2,4 +2,5 @@
 
 source .venv/bin/activate
 
+alembic -c conf/alembic.ini upgrade head
 uvicorn app.main.web_api:create_app --factory --lifespan on
--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -2,4 +2,5 @@
 
 source .venv/bin/activate
 
+alembic -c conf/alembic.ini upgrade head
 gunicorn -c conf/gunicorn.conf.py 'app.main.web_api:create_app()'
